ca244e49bff7cc3bb36d23fa1bb3ec510895dd86
Dylan Scotney <ucapdls@ucl.ac.uk>
2021-01-31 12:45:21 +0000

    Add select_all_by_delivery_date to DB_queries

---
+++ tests/test_get_biweekly_coffee_requirements.py
@@ -0,0 +1,15 @@
+import datetime as dt
+
+import lib.DB.DB_queries as DB
+import lib.PackageConfig as config 
+
+conn = DB.create_connection(config.ORDERS_DB_LOC)
+
+testdate = dt.datetime(2021, 1, 16)
+enddate = testdate + dt.timedelta(days=1)
+
+orders = DB.select_all_by_delivery_date("CompressedOrderHistory", testdate, testdate, conn)
+
+print(orders)
+
+orders.to_csv("C:/Users/dylan/Documents/test/20210116_orders.csv")
