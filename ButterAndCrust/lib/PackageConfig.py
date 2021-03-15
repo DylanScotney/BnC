48861147d9f35be40186254e59424e1557119e94
Dylan Scotney <ucapdls@ucl.ac.uk>
2021-03-15 20:02:57 +0000

    Prod package config

--- ButterAndCrust/lib/PackageConfig.py
+++ ButterAndCrust/lib/PackageConfig.py
@@ -4,23 +4,23 @@ import posixpath
 import ButterAndCrust
 import ButterAndCrust.images
 
-DEVELOPMENT = True
+DEVELOPMENT = False
 
 # Airtable keys.
-base_key = "appWCrLerVduIq5SR" if DEVELOPMENT else ""
-api_key = "keyQNWjWomXyaBGJK" if DEVELOPMENT else ""
+base_key = "appWCrLerVduIq5SR" if DEVELOPMENT else "appPhU88MDepiAjpR"
+api_key = "keyQNWjWomXyaBGJK" if DEVELOPMENT else "keyEabJJ9ddj5UNsA"
 
 # Location of cold storage DB
-DB_BASE_PATH = "C:/Users/dylan/Documents/Programming/ButterAndCrust/DB/"
+DB_BASE_PATH = "/Users/olliebrenman/Desktop/Butter/code/DB/"
 ORDERS_DB_NAME = "OrderHistory"
 COLD_STORAGE_ORDERS_DB_LOC = DB_BASE_PATH + ORDERS_DB_NAME + ".db"
 
 # Must install [https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_msvc2015-win64.exe]
-PATH_WKHTMLTOPDF = "C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe"
+PATH_WKHTMLTOPDF =  "/Users/olliebrenman/Desktop/Butter/code/wkhtmltopdf"
 
 # Default output location for package slips and stock requirements 
 # this can also be changed on the command line
-DEFAULT_OUTPUT_LOCATION = "C:/Users/dylan/Documents/test/"
+DEFAULT_OUTPUT_LOCATION = "/Users/olliebrenman/Desktop/Butter/code/Output/"
 
 # A temporary working directory that will store files
 WORKING_DIRECTORY = posixpath.join(os.path.dirname(ButterAndCrust.__file__), "WorkingDirectory/")
