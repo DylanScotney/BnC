2403ef039d95d156a7c875b31daa8f4b3e562ad3
Dylan Scotney <ucapdls@ucl.ac.uk>
2021-03-21 12:26:31 +0000

    Remove unused files

--- ButterAndCrust/lib/DB/credentials.py
+++
@@ -1,5 +0,0 @@
-from os.path import dirname, abspath
-
-DB_BASE_PATH = "C:/Users/dylan/Documents/Programming/ButterAndCrust/DB/"
-ORDERS_DB_NAME = "OrderHistory"
-ORDERS_DB_LOC = DB_BASE_PATH + ORDERS_DB_NAME + ".db"
